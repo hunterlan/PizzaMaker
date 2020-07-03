00ef0fd35fb7a5ad3dc5f596db28b07bfe4e1fde
hunterlan <kotyshe@gmail.com>
2020-07-03 23:35:26 +0300

    Changed connection

--- PizzaMaker/PizzaMaker/Context/PizzaContext.cs
+++ PizzaMaker/PizzaMaker/Context/PizzaContext.cs
@@ -40,7 +40,7 @@ namespace PizzaMaker.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql(GetConnection("DeveloperRemoteDB"));
+            optionsBuilder.UseMySql(GetConnection("DeveloperHomeDB"));
         }
     }
 }
