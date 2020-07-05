96a63839ecdca27c47c78b1f47a51939c6670cfb
hunterlan <kotyshe@gmail.com>
2020-07-05 13:45:49 +0300

    Starting working with orders

---
+++ PizzaMaker/PizzaMaker/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaMaker.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public string NameReciver { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int PizzaID { get; set; }
+        public int Count { get; set; }
+    }
+}
